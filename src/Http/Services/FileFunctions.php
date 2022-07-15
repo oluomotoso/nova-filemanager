f4007f95b8d0b16f64588db900c9d891a1af1fcd
Olumide Omotoso <oluomotoso@gmail.com>
2022-07-15 17:31:31 +0100

    Update FileFunctions.php

--- src/Http/Services/FileFunctions.php
+++ src/Http/Services/FileFunctions.php
@@ -29,10 +29,10 @@ trait FileFunctions
         $unit = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
         $times = floor(log($size, $base));
         //this is required to fix division by zero error
-        $sizeDiv=pow($base, ($times + $level)));
+        $sizeDiv=pow($base, ($times + $level));
         $sizeDiv = $sizeDiv>0?$sizeDiv:1;
 
-        return sprintf('%.'.$precision.'f', $size / $sizeDiv.' '.$unit[$times + $level];
+        return sprintf('%.'.$precision.'f', $size / $sizeDiv).' '.$unit[$times + $level];
     }
 
     /**
